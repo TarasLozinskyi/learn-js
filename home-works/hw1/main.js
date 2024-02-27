2f6c76a403fdc6a041afd9b93fa3c8a3fc5f8d49
TarasLozinskyi <tlozinskyi@gmail.com>
2024-02-27 22:17:58 +0200

    did the first homework

---
+++ home-works/hw1/main.js
@@ -0,0 +1,68 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//   Вивести кожну змінну за допомогою: console.log
+
+let hello = 'hello';
+let owu = 'ouw';
+let com ='com';
+let ua = 'ua';
+let num1= 1;
+let num2 = 10;
+let num3 = -999;
+let num4 = 123;
+let num5 = 3.14;
+let num6 = 2.7;
+let num7 = 16;
+let bool1 = true;
+let bool2 = false;
+
+console.log(hello);
+console.log(owu);
+console.log(com);
+console.log(ua);
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(num5);
+console.log(num6);
+console.log(num7);
+console.log(bool1);
+console.log(bool2);
+console.log('---------------------------------');
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
+let firstName ='Taras';
+let middleName ='Oleksiyovych';
+let lastName = 'Lozinskyi';
+
+// let person = firstName + ' ' + lastName + ' '+ middleName;
+let person = `${firstName} ${lastName} ${middleName}`;
+console.log(person);
+console.log('---------------------------------');
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+
+let a =100;
+let b = '100';
+let c =true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log('---------------------------------');
+
+// Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+let name = prompt('Ваше імя',);
+let lName = prompt('Ваше прізвище');
+let age = prompt('Скільки вам років?');
+
+console.log(name);
+console.log(lName);
+console.log(age);
+
+
+
